e6d07710f04564bf6c795c9198dda61e1f8dd490
Cihan Bulut <bulutcihan1970@gmail.com>
2022-03-08 17:39:31 -0500

    signpage and signingstepdefs updated

--- src/test/java/pages/SignInPage.java
+++ src/test/java/pages/SignInPage.java
@@ -1,6 +1,32 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class SignInPage {
 
+    public SignInPage() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
 
-}
+
+        @FindBy(id = "account-menu")
+        public WebElement loginDropdown;
+
+        @FindBy(xpath = "//*[contains(text(),'Sign in')]")
+        public WebElement signDropdown;
+
+        @FindBy(id = "username")
+        public WebElement username;
+
+        @FindBy(id = "password")
+        public WebElement password;
+
+        @FindBy(xpath = "//button[@type='submit']")
+        public WebElement loginButton;
+
+
+    }
+
