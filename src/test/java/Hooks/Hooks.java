685b0f962e91b35b5c05b3116ab933c028268962
esra <esra_izgi@hotmail.com>
2022-03-09 12:38:13 -0800

    Web elements for DoctorPage added

--- src/test/java/Hooks/Hooks.java
+++ src/test/java/Hooks/Hooks.java
@@ -17,9 +17,6 @@ public class Hooks {
 
     }
 
-
-
-
     public static RequestSpecification spec;
 
     @Before( value = "@ApiRegistrant")
--- src/test/java/pages/DoctorPage.java
+++ src/test/java/pages/DoctorPage.java
@@ -1,4 +1,38 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+import java.util.List;
+
 public class DoctorPage {
+
+
+    public DoctorPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(id = "entity-menu")
+    public WebElement MyPagesDropDown;
+
+    @FindBy(xpath =  "(//*[contains(text(),'My Appointments')])[1]")
+    public WebElement MyAppointments;
+
+    @FindBy(xpath = "//*[contains(text(),'Request Inpatient')]")
+    public WebElement RequestInpatient;
+
+    @FindBy(id = "fromDate")
+    public WebElement fromDateBox;
+
+    @FindBy(id = "toDate")
+    public WebElement toDateBox;
+
+    @FindBy(xpath = "//div[@class='btn-group flex-btn-group-container']")
+    public List<WebElement> editList;
+
+    @FindBy(xpath = "(//div[@class='btn-group flex-btn-group-container'])[1]")
+    public WebElement  edit1;
+
 }
