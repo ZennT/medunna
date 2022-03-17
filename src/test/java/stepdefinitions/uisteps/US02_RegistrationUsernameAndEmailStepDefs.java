d4f358b7477fac49d43ba57eedf619e0d6bbd7f3
esra <esra_izgi@hotmail.com>
2022-03-16 22:33:01 -0700

    update

---
+++ src/test/java/stepdefinitions/uisteps/US02_RegistrationUsernameAndEmailStepDefs.java
@@ -0,0 +1,219 @@
+package stepdefinitions.uisteps;
+
+import com.github.javafaker.Faker;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import pages.RegistrationPage;
+import pages.SignInPage;
+import pojos.Registrant;
+import utilities.ConfigurationReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+import java.io.IOException;
+import java.util.List;
+import static utilities.WriteToTxt.saveRegistrantData;
+
+public class US02_RegistrationUsernameAndEmailStepDefs {
+
+    Registrant registrant = new Registrant();
+    Faker faker = new Faker();
+    RegistrationPage registrationPage = new RegistrationPage();
+    SignInPage signInPage = new SignInPage();
+
+
+
+    @Given("user is on homepage")
+    public void userIsOnHomepage() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("medunna_login_url"));
+    }
+
+
+    @And("user clicks on sign in register dropdown")
+    public void userClicksOnSignInRegisterDropdown() {
+       Driver.waitAndClick(signInPage.loginDropdown);
+    }
+
+    @And("user clicks on Register button")
+    public void userClicksOnRegisterButton() {
+        Driver.waitAndClick(signInPage.registerButton);
+
+    }
+
+    @And("user is on registration page and verifies")
+    public void userIsOnRegistrationPageAndVerifies() {
+        registrationPage.registrationText.isDisplayed();
+
+    }
+
+
+    @Given("user provides ssn {string}")
+    public void user_provides_ssn_id(String ssn) {
+        ssn = faker.idNumber().ssnValid();
+        registrant.setSsn(ssn);
+        Driver.waitAndSendText(registrationPage.ssnTextbox,ssn);
+
+    }
+    @Then("user provides firstname and lastname {string} and {string}")
+    public void user_provides_firstname_and_lastname_and(String firstname, String lastname) {
+        firstname =faker.name().firstName();
+        lastname = faker.name().lastName();
+
+        registrant.setFirstName(firstname);
+        registrant.setLastName(lastname);
+        Driver.waitAndSendText(registrationPage.firstNameTextBox,firstname);
+        Driver.waitAndSendText(registrationPage.lastNameTextBox,lastname);
+
+    }
+    @Then("user provides username {string}")
+    public void user_creates_username(String username) {
+//        username = registrantpojo.getFirstName() + registrantpojo.getLastName(); // burada ikisini birlestirdik firstname and lastname
+//        registrantpojo.setUserName(username);                                        usernamei olustirduk
+
+        username=faker.name().username();
+        registrant.setUserName(username);
+        Driver.waitAndSendText(registrationPage.usernameTextBox,username);
+    }
+    @Then("user provides email {string}")
+    public void user_provides_also_email(String email) {
+        email =faker.internet().emailAddress();
+        registrant.setUserName(email);
+        Driver.waitAndSendText(registrationPage.emailTextBox,email);
+
+
+    }
+    @Then("user provides {string}")
+    public void user_generates_the_password(String password) {
+      password =faker.internet().password(8,20,true,true);
+        registrant.setPassword(password);
+
+      Driver.waitAndSendText(registrationPage.newPasswordTextBox, password);
+      Driver.waitAndSendText(registrationPage.newPasswordConfirmRequiredText,password);
+    }
+    @Then("user registers and validates")
+    public void user_registers_and_validates() {
+        Driver.waitAndClick(registrationPage.registerButton);
+        Assert.assertTrue(Driver.waitForVisibility(registrationPage.successMessageToastContainer,2).isDisplayed());
+    }
+
+    @Then("user creates the records to a correspondent file")
+    public void user_creates_the_records_to_a_correspondent_file() {
+
+        saveRegistrantData(registrant);
+    }
+
+
+
+
+
+
+
+    @Given("user provides space in username")
+    public void userProvidesSpaceInUsername() {
+        registrationPage.usernameTextBox.sendKeys("molly-19 me");
+        registrationPage.usernameTextBox.sendKeys(Keys.ENTER);
+}
+
+    @And("user should see Your username is invalid. text visible and verifies")
+    public void userShouldSeeYourUsernameIsInvalidTextVisibleAndVerifies() {
+        Assert.assertTrue(registrationPage.invalidUserNametext.isDisplayed());
+    }
+
+
+
+    @Then("capture the screenshot")
+    public void capture_the_screenshot() throws IOException {
+        ReusableMethods.getScreenshot("ScreenShoots");
+    }
+
+    @Then("close the application")
+    public void close_the_application() {
+        Driver.closeDriver();
+    }
+
+
+    @When("user provides characters in username {string}")
+    public void userProvidesInvalidCharactersInUsername(String username) {
+        Driver.waitAndSendText(registrationPage.usernameTextBox, username);
+        registrationPage.usernameTextBox.sendKeys(Keys.ENTER);
+    }
+
+
+
+    @And("user leaves blank for username of the applicant")
+    public void userLeavesBlankForUsernameOfTheApplicant() {
+        registrationPage.usernameTextBox.sendKeys(" ");
+        registrationPage.usernameTextBox.sendKeys(Keys.ENTER);
+    }
+
+    @And("verifies Your username is required. text visible")
+    public void verifiesYourUsernameIsRequiredTextVisible() {
+        Assert.assertTrue(registrationPage.requiredUserNameText.isDisplayed());
+
+    }
+
+    @Given("user provides invalid {string} and click enter")
+    public void userProvidesInvalidAndClickEnter(String emailAddress) throws IOException {
+        registrant.setEmail(emailAddress);
+        Driver.waitAndSendText(registrationPage.emailTextBox, emailAddress);
+        //   registrationPage.emailTextbox.sendKeys(emailAddress);
+        registrationPage.emailTextBox.sendKeys(Keys.ENTER);
+        ReusableMethods.getScreenshot("Wrong email Address");
+
+
+    }
+
+    @And("verifies This field is invalid text visible")
+    public void verifiesThisFieldIsInvalidTextVisible() throws IOException {
+        Assert.assertTrue(registrationPage.invalidThisFieldIsText.isDisplayed());
+        registrationPage.emailTextBox.clear();    // ayni senaryoda oldugu icin clear yapmali
+        ReusableMethods.getScreenshot("invalid text visibility");
+    }
+
+    @And("user leaves blank for emailAddress of the applicant")
+    public void userLeavesBlankForEmailAddressOfTheApplicant() {
+        registrationPage.emailTextBox.sendKeys(" ");
+        registrationPage.emailTextBox.sendKeys(Keys.ENTER);
+
+    }
+    @And("verifies Your email is required. text visible")
+    public void verifiesYourEmailIsRequiredTextVisible() {
+        Assert.assertTrue(registrationPage.requiredEmailText.isDisplayed());
+    }
+
+
+
+
+    @And("user provides already used email {string}")
+    public void userProvidesAlreadyUsedEmail(String usedEmail) {
+        Driver.waitAndSendText(registrationPage.emailTextBox,usedEmail);
+    }
+
+    @And("user verifies that email already used")
+    public void userVerifiesThatEmailAlreadyUsed() {
+        Driver.waitAndClick(registrationPage.registerButton);
+        //   Assert.assertTrue(registrationPage.emailAlreadyUsedToastContainer.isDisplayed());
+        Assert.assertTrue(Driver.waitForVisibility(registrationPage.emailAlreadyUsedToastContainer,2).isDisplayed());
+
+    }
+
+    @And("user provides username that already used {string}")
+    public void userProvidesUsernameThatAlreadyUsed(String usedUsername) {
+        Driver.waitAndSendText(registrationPage.usernameTextBox,usedUsername);
+    }
+
+    @And("user verifies that username already used")
+    public void userVerifiesThatUsernameAlreadyUsed() {
+        Driver.waitAndClick(registrationPage.registerButton);
+        Assert.assertTrue(Driver.waitForVisibility(registrationPage.loginAlreadyUsedToastContainer,2).isDisplayed());
+    }
+
+    }
+
+
+
+
---
+++ src/test/resources/features/UIFeatures/US02_RegistirationUsernameAndEmail.feature
@@ -0,0 +1,83 @@
+@UIRegistrationPage @smoke @UIHome
+Feature: registration feature
+
+  Background: user is on the registration page
+    Given user is on homepage
+    And user clicks on sign in register dropdown
+    And user clicks on Register button
+    And user is on registration page and verifies
+
+
+  @UIRegistration_US002_TC0001_TC0006
+  Scenario Outline: positive testing with all valid credentials
+    And user provides ssn "<SSN>"
+    Then user provides firstname and lastname "<firstname>" and "<lastname>"
+    And user provides username "<username>"
+    And user provides email "<email>"
+    And user provides "<password>"
+    And user registers and validates
+    Then user creates the records to a correspondent file
+    Examples:
+      | SSN         | firstname | lastname | username    | email           | password   |
+      | 170-98-8790 | New User  | last     | hel.ly_can1 | molly6@gmail.com | mollyA123. |
+
+  @UIRegistration_US002_TC0005 @smoke
+  Scenario Outline: user should not able register with Username that already used
+    And user provides ssn "<SSN>"
+    Then user provides firstname and lastname "<firstname>" and "<lastname>"
+    And user provides username that already used "<username>"
+    And user provides email "<email>"
+    And user provides "<password>"
+    And user verifies that username already used
+    Then capture the screenshot
+    Then user creates the records to a correspondent file
+    Examples:
+      | SSN         | firstname | lastname | username    | email         | password |
+      | 387-98-8790 | New       | name     | hel.ly_can1 | mol@gmail.com | newA123. |
+
+
+  @UIRegistration_US002_TC_0011
+  Scenario Outline: user should not able register with Email that already used
+    And user provides ssn "<SSN>"
+    Then user provides firstname and lastname "<firstname>" and "<lastname>"
+    And user provides username "<username>"
+    And user provides already used email "<email>"
+    And user provides "<password>"
+    And user verifies that email already used
+    Then capture the screenshot
+    Then user creates the records to a correspondent file
+    Examples:
+      | SSN         | firstname | lastname | username       | email            | password |
+      | 387-98-8790 | Newt      | nameff   | hello.ly5_can1 | molly6@gmail.com | nttA123. |
+
+
+
+  @UIRegistrationNegativeTesting_US002_TC0002
+  Scenario: user should not able to register with invalid Username
+    Given user provides space in username
+    And user should see Your username is invalid. text visible and verifies
+    And capture the screenshot
+    Then close the application
+
+
+
+  @UIRegistration_US002_TC0004 @smoke
+    Scenario: user should not leave username blank
+    And user leaves blank for username of the applicant
+    And verifies Your username is required. text visible
+
+
+
+  @UIRegistrationNegativeTestingTC_0007_TC_0008_TC_0009_TC_0010
+ Scenario Outline: user should not able to register with invalid EmailAddress
+    Given user provides invalid "<invalid emailAddress>" and click enter
+    And verifies This field is invalid text visible
+    And user leaves blank for emailAddress of the applicant
+    And verifies Your email is required. text visible
+    Then close the application
+    Examples:
+      | invalid emailAddress |
+      | mollygmail.com       |
+      | molly@gmail          |
+      | mollygmail           |
+
