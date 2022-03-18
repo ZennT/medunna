c97e3713b1d07cf561f4dca65cc825001b5f58ff
mrtriver <mrtriver@gmail.com>
2022-03-18 12:47:22 -0400

    US20 son

--- src/test/java/Hooks/Hooks.java
+++ src/test/java/Hooks/Hooks.java
@@ -22,7 +22,7 @@ public class Hooks {
     //
     public static RequestSpecification spec;
 
-    @Before( value = "@ApiRegistrant")
+    @Before( value = "@PatientInfoApi")
     public void setup(){
 
         spec = new RequestSpecBuilder().setBaseUri(ConfigurationReader.getProperty("base_url")).build();
--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "./src/test/resources/features",
         glue = {"stepdefinitions","Hooks"},
 
-        tags = "@UIlogin",
+        tags = "@User_Management_UI",
 
 
 
