98d8876fcd5aff0865fcbe185809e49b7de21fa9
ZennT <zennt@gmail.com>
2022-03-22 15:14:09 -0700

    new method in Driver

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -315,4 +315,12 @@ public class Driver {
     public static void waitAndClickLocationText(WebElement element, String value) {
         Driver.getDriver().findElement(By.xpath("//*[text()='" + value + "']")).click();
     }
+
+
+
+    public static void clickElementByJS(WebElement element) {
+        // javascript executor click
+        JavascriptExecutor jsexecutor = ((JavascriptExecutor) Driver.getDriver());
+        jsexecutor.executeScript("arguments[0].click();", element);
+    }
 }
